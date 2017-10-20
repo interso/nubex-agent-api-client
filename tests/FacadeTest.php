850b5eec3948048b7eaedceee7916bb63d24ce5d
Vasilev Sergei <vasilev@interso.ru>
2017-10-20 15:17:55 +0300

    Добавление теста для проверки списка прототипов

--- tests/FacadeTest.php
+++ tests/FacadeTest.php
@@ -2,17 +2,25 @@
 
 namespace Interso\Tests;
 
+use Interso\NubexAgentAPI\DTO\PrototypeDTO;
 use Interso\NubexAgentAPI\DTO\SiteDTO;
 use Interso\NubexAgentAPI\Facade;
 use PHPUnit\Framework\TestCase;
 
 class FacadeTest extends TestCase
 {
-    public function testCreate()
+    public function testSiteList()
     {
         $facade = new Facade('http://symfony4.app/api/v1', '222');
         $list = $facade->sites()->getList();
         $this->assertInstanceOf(SiteDTO::class, $list[0]);
+    }
+
+    public function testPrototypeList()
+    {
+        $facade = new Facade('http://symfony4.app/api/v1', '222');
+        $list = $facade->prototypes()->getList();
+        $this->assertInstanceOf(PrototypeDTO::class, $list[0]);
     }
 }
 
